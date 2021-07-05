c72bed19e9b26107b27a9e8a423f4dae6216a6ac
thinhbuihong <thinhbuihong@gmail.com>
2021-07-05 11:13:42 +0700

    reading and writeng files

---
+++ writing-files.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	d1 := []byte("hello\nthinh dep zai")
+	err := ioutil.WriteFile("./writeFiles", d1, 0644)
+	check(err)
+
+	f, err := os.Create("./write2.txt")
+	check(err)
+
+	defer f.Close()
+
+	d2 := []byte{155, 111, 10, 34, 67}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes string\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n3)
+
+	f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n4)
+
+	w.Flush()
+}
