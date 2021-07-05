eb127bf41378a7b48cd6c4f9ec332e057bf346b2
thinhbuihong <thinhbuihong@gmail.com>
2021-07-05 22:04:38 +0700

    testing

---
+++ file-paths.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	p := filepath.Join("dir1", "dir2", "filename")
+	fmt.Println("p: ", p)
+
+	fmt.Println(filepath.Join("dir1//", "filename"))
+	fmt.Println(filepath.Join("dir1/../dir1", "filename"))
+
+	fmt.Println("Dir(p):", filepath.Dir(p))
+	fmt.Println("Base(p):", filepath.Base(p))
+
+	fmt.Println(filepath.IsAbs("dir/file"))
+	fmt.Println(filepath.IsAbs("/dir/file"))
+
+	fileName := "config.json"
+	ext := filepath.Ext(fileName)
+	fmt.Println(ext)
+
+	fmt.Println(strings.TrimSuffix(fileName, ext))
+
+	rel, err := filepath.Rel("a/b", "a/b/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+	rel, err = filepath.Rel("a/b", "a/c/t/file")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(rel)
+}
